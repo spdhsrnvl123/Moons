d06798b530d6e6628f65229ea104578df55ab71e
leutbing <spdhsrnvl456@gmail.com>
2025-07-09 12:45:32 +0900

    feat:헤더 공통 컴포넌트 구현

---
+++ src/components/header.js
@@ -0,0 +1,31 @@
+export const create_header = () => {
+  const header = document.createElement("header");
+  header.innerHTML = `
+    <div class="menu">
+      <h1>
+        <a href="../pages/home/index.html">Moon:s</a>
+      </h1>
+      <ul class="menu_content">
+        <li>
+          <a href="../pages/about/index.html">About</a>
+        </li>
+        <li>
+          <a href="../pages/service/index.html">Service</a>
+        </li>
+        <li>
+          <a href="../pages/media/index.html">Media</a>
+        </li>
+      </ul>
+    </div>
+    `;
+    return header;
+};
+
+
+export const init_header = ()=>{
+    const header_container = document.querySelector("#header-container");
+
+    if(header_container){
+        header_container.appendChild(create_header());
+    }
+}
